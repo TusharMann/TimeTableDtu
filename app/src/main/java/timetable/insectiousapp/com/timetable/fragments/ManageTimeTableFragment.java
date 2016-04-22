597f9b5f356725b581b68b29b63838adfcb2ce46
chetancodev95 <chetanmann95@gmail.com>
2016-04-22 18:34:45 +0530

    updated credentials and shared preferences part

--- app/src/main/java/timetable/insectiousapp/com/timetable/fragments/ManageTimeTableFragment.java
+++ app/src/main/java/timetable/insectiousapp/com/timetable/fragments/ManageTimeTableFragment.java
@@ -2,15 +2,18 @@ package timetable.insectiousapp.com.timetable.fragments;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import timetable.insectiousapp.com.timetable.R;
 import timetable.insectiousapp.com.timetable.activities.SetWriteKeyActivity;
+import timetable.insectiousapp.com.timetable.others.SharedPreferencesFiles;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,20 +25,44 @@ public class ManageTimeTableFragment extends Fragment implements View.OnClickLis
         // Required empty public constructor
     }
 
-
-   // View v;
+    View v;
     ImageView ivEditWriteKey;
+    TextView tvClassId, tvWriteKey;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v= inflater.inflate(R.layout.fragment_manage_time_table, container, false);
+         v= inflater.inflate(R.layout.fragment_manage_time_table, container, false);
 
         ivEditWriteKey=(ImageView)v.findViewById(R.id.fragment_manage_time_table_iv_editwritekey);
+        tvClassId=(TextView)v.findViewById(R.id.class_detail_layout_2_tv_classid);
+        tvWriteKey=(TextView)v.findViewById(R.id.class_detail_layout_2_tv_writekey);
         ivEditWriteKey.setOnClickListener(this);
 
+
+
         return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setCredentialsFromSharedPreferences();
+    }
+
+    private void setCredentialsFromSharedPreferences() {
+        SharedPreferencesFiles SPF=new SharedPreferencesFiles();
+        //for class id
+        SharedPreferences sharedPreferences=getActivity().getSharedPreferences(SPF.getSPClassId(), 0);
+        String classId=sharedPreferences.getString(SPF.getClassId(), "Not set yet");
+        //
+        //for write key
+        SharedPreferences sharedPreferences2=getActivity().getSharedPreferences(SPF.getSPWriteKey(), 0);
+        String writeKey=sharedPreferences2.getString(SPF.getWriteKey(), "Not set yet");
+        //
+        tvClassId.setText(classId);
+        tvWriteKey.setText(writeKey);
     }
 
     @Override
