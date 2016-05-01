166b10bf2ef6f51f064123caf75fbbae8a1f0435
chetancodev95 <chetanmann95@gmail.com>
2016-05-01 23:53:52 +0530

    added dynamic colors

--- app/src/main/java/timetable/insectiousapp/com/timetable/others/ClassroomListAdapter.java
+++ app/src/main/java/timetable/insectiousapp/com/timetable/others/ClassroomListAdapter.java
@@ -1,14 +1,16 @@
 package timetable.insectiousapp.com.timetable.others;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
+import java.util.Random;
 
 import timetable.insectiousapp.com.timetable.R;
 
@@ -20,6 +22,10 @@ public class ClassroomListAdapter extends ArrayAdapter<Classroom> {
     LayoutInflater l;
     Context context;
     List<Classroom> objects;
+    LinearLayout llColorBackground;
+    Random crazy;
+
+    int[] colors={R.color.bluelight, R.color.grey4, R.color.grey2, R.color.darkblue4, R.color.orange2, R.color.lightgreen1};
 
     public ClassroomListAdapter(Context context, int resource, List<Classroom> objects, LayoutInflater l) {
         super(context, resource, objects);
@@ -37,7 +43,12 @@ public class ClassroomListAdapter extends ArrayAdapter<Classroom> {
 
         TextView tvId=(TextView)v.findViewById(R.id.all_classroom_id);
         TextView tvName=(TextView)v.findViewById(R.id.all_classroom_name);
+        llColorBackground=(LinearLayout)v.findViewById(R.id.all_classroom_ll_color);
 
+        crazy=new Random();
+        llColorBackground.setBackgroundResource(colors[crazy.nextInt(colors.length-1)]);
+        Log.i("colorvalues", "crazy.nextInt :"+crazy.nextInt(colors.length-1));
+        Log.i("colorvalues", "color :"+colors[crazy.nextInt(colors.length-1)]);
         Classroom c1=objects.get(position);
         tvId.setText(c1.id);
         tvName.setText(c1.name);
