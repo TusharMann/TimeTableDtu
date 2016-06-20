63298f43976bb10b7e2a62580d7eae9acf8b4595
chetancodev95 <chetanmann95@gmail.com>
2016-06-20 23:17:09 +0530

    added weektimetableactivity

---
+++ app/src/main/java/timetable/insectiousapp/com/timetable/activities/WeekTimeTableActivity.java
@@ -0,0 +1,20 @@
+package timetable.insectiousapp.com.timetable.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import timetable.insectiousapp.com.timetable.R;
+
+public class WeekTimeTableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_week_time_table);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
